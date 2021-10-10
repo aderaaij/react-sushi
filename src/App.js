1bd22a58995e4703410f4f9282232ef6d91ccd04
Arden de Raaij <arden.de.raaij@plumguide.com>
2021-10-10 16:56:45 +0100

    sushi scene

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from 'react'
+import Sushi from './components/sushi'
+import { Canvas } from '@react-three/fiber'
 
-import { Html, OrbitControls } from "@react-three/drei";
+import { Html, OrbitControls } from '@react-three/drei'
 function App() {
   return (
     <Canvas camera={{ fov: 45, position: [-8, 6, -8] }}>
-    <color attach="background" args={["#98ddca"]} />
-    <Suspense fallback={<Html center>loading...</Html>}>
-      <ambientLight intensity={1} />
-      <group
-        position={[9.46, 13.38, -9.46]}
-        rotation={[0, Math.PI / 4, -Math.PI / 4]}
-        scale={[1.65, 1.65, 1.65]}
-      >
-        <directionalLight
-          intensity={3}
-          decay={0}
-          rotation={[Math.PI / 2, 0, 0]}
-        />
-        <spotLight intensity={0.5}></spotLight>
-      </group>
-     
-    </Suspense>
-    {/* <OrbitControls /> */}
-  </Canvas>
-  );
+      <color attach="background" args={['#FFB002']} />
+      <Suspense fallback={<Html center>loading...</Html>}>
+        <ambientLight intensity={1} />
+        <group
+          position={[9.46, 13.38, -9.46]}
+          rotation={[0, Math.PI / 4, -Math.PI / 4]}
+          scale={[1.65, 1.65, 1.65]}
+        >
+          <directionalLight
+            intensity={3}
+            decay={0}
+            rotation={[Math.PI / 2, 0, 0]}
+          />
+          <spotLight intensity={0.5}></spotLight>
+        </group>
+        <Sushi />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
